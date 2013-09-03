566da9416aa20bda3c5deb619e3d0c2f1b28b45e
Felipe <felipepqm@gmail.com>
2013-09-03 20:36:52 -0300

    Commit inicial projeto

--- application/controllers/cadastro.php
+++ application/controllers/cadastro.php
@@ -1,10 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Cadastro extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view("pagina-inicial");
+		$this->load->view("cadastro");
 	}
 	
 }
---
+++ application/views/cadastro.php
@@ -0,0 +1,8 @@
+<?php $this->load->view('header') ?>	
+<body>
+		
+VISH MANO		
+<?php $this->load->view('footer') ?>		
+</body>
+	
+</html>
