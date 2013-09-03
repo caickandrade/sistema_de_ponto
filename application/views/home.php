b61a6aae9283b3d09a9f050065108b139caa4428
Joana <joanagabrielar@yahoo.com>
2013-09-02 21:27:25 -0300

    escrevi ei

--- application/views/home.php
+++ application/views/home.php
@@ -1,27 +1,124 @@
 <html>
-<head>
+	
+	<head>
+		<title>Home</title>
+		<script src = "<?php echo base_url('/assets/js/jquery.js')?>"> </script>
+		<script src = "<?php echo base_url('/assets/js/cadastro.js')?>"> </script>
+	</head>
+	
+	<body>
+		
+		<p class="cadastro"> Cadastro </p>
+		<br />
+		Ei ! 
+		
+		Nome
+		<input type="text" name="nome" id="nome" class="nome" />
+		
+		Email
+		<input type="email" name="email" id="email" class="email" />
+		<br />
+		
+		Senha
+		<input type="password" name="senha" id="senha" class="senha" />
+		<br />
+		
+		Funcao
+		<input type="text" name="funcao" id="funcao" class="funcao" />
+		<br />
+		
+		Rua
+		<input type="text" name="rua" id="rua" class="rua" />
+		
+		Numero
+		<input type="number" name="numero" id="numero" class="numero" />
+		
+		Complemento
+		<input type="text" name="comp" id="comp" class="comp" />
 
-<meta charset='utf-8'/>
-   <link rel='stylesheet' href='assets/css/style.css' />
-   <style type="text/css">
+		Bairro
+		<input type="text" name="bairro" id="bairro" class="bairro" />
+		<br />
+		
+		CEP
+		<input type="number" name="cep" id="cep" class="cep" />
+		
+		Cidade
+		<input type="text" name="cidade" id="cidade" class="cidade" />
+		
+		Estado
+		<input type="text" name="estado" id="estado" class="estado" />
+		<br />
+		
+		Telefone 1
+		<input type="number" name="tel1" id="tel1" class="tel1" />
+		
+		Telefone 2
+		<input type="number" name="tel2" id="tel2" class="tel2" />
+		<br />
+		
+		<input type="submit" value="Cadastrar" class="botao" id="botao" />
+		
+		<p>Deletar</p>
+		<br />
+		ID
+		<input type="number" name="iddel" id="iddel" class="iddel" />
+		
+		<input type="submit" value="Deletar" class="del" id="del" />
+		<br />
+		<p> Update </p>
+		<br />
+		ID<br/>
+		<input type="number" name="idup" id="idup" class="idup" />
+		Nome
+		<input type="text" name="nome" id="nome" class="nome" />
+		
+		Email
+		<input type="email" name="email" id="email" class="email" />
+		<br />
+		
+		Senha
+		<input type="password" name="senha" id="senha" class="senha" />
+		<br />
+		
+		Funcao
+		<input type="text" name="funcao" id="funcao" class="funcao" />
+		<br />
+		
+		Rua
+		<input type="text" name="rua" id="rua" class="rua" />
+		
+		Numero
+		<input type="number" name="numero" id="numero" class="numero" />
+		
+		Complemento
+		<input type="text" name="comp" id="comp" class="comp" />
 
-
-</style>
-</head>
-<body>
-
- <div id ="centro">
- <form method="post" action="">
- 
-<div id="login"> 
-<label for="login"> Login : </label>  <input  type=text required name=login>
-</div>
-<div id="senha">
-<label for="password">Senha : </label><input  id="password" type="password"  required name=password> 
-<input type=submit value="Entrar"/>
-</div>
-</form>
-</div>
-</body>
+		Bairro
+		<input type="text" name="bairro" id="bairro" class="bairro" />
+		<br />
+		
+		CEP
+		<input type="number" name="cep" id="cep" class="cep" />
+		
+		Cidade
+		<input type="text" name="cidade" id="cidade" class="cidade" />
+		
+		Estado
+		<input type="text" name="estado" id="estado" class="estado" />
+		<br />
+		
+		Telefone 1
+		<input type="number" name="tel1" id="tel1" class="tel1" />
+		
+		Telefone 2
+		<input type="number" name="tel2" id="tel2" class="tel2" />
+		<br />
+		
+		<input type="submit" value="Atualizar" class="botao" id="update" />
+		
+				
+		
+	</body>
+	
 </html>
-
