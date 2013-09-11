379f3a64cd70212e615eef3eee5b2e3d2fc04f49
Joana <joanagabrielar@yahoo.com>
2013-09-10 21:08:21 -0300

    eu adicionei o bootstrap ao cadastro alm de inclui-lo dentro do header e tambm atualizei o autoload

--- application/views/cadastro.php
+++ application/views/cadastro.php
@@ -2,89 +2,138 @@
 	<head>
 		<meta charset="UTF-8" />
 		<title>Dados Pessoais</title>
+		<!-- <link href="C:/wamp/www/sistema_de_ponto/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"> -->
+
 	</head>
-	<?php $this->load->view('header') ?>	
+	<?php $this->load->view('header') ?>
 
 	<body>
-		<form>
-				<p><label for= 'a' >Cadastro</label></p>
-				
-				<p><label for='a'>Name </label>
-					<input type='text' name='Name' id='a'/></p>
-					
-				<p><label for='b'>Email </label>
-					<input type='email' name='email' id='b'/></p>	
-				
-				<p><label for='L'>Senha </label>
-					<input type='password' name='senha' id='L'/></p>
-					
-				<p><label for='c'>Phone</label>
-					<input type='string' name='phone' id='c'/></p>
-					
-				<p><label for='d'>Phone 2 </label>
-					<input type='string' name='phone2' id='d'/></p>
-					
-				<p><label for='j'>Data de Nascimento</label>
-					<input type="string" name='nascimento' id='j'/></p>
-					
-				<p><label for='k'>CEP</label>
-					<input type="string" name='cep' id='k'/></p>
-				
-				<p><label for='l'>Função</label>
-					<input type="string" name='funcao' id='l'/></p>
-										
-															
-					<p><label for='e'>Endereço</label></p> 
-					
-				<p><label for='e'>Rua </label>
-					<input type='text' name='rua' id='e'/></p>			
-					
-				<p><label for='numero'>Número</label>
-					<input type='string' name= 'numero' id='numero'/></p>
-					
-				<p><label for='g'>Complemento</label>
-					<input type='text' name= 'complemento' id='g'/></p>
-										
-				<p><label for='h'>Bairro</label>
-					<input type='text' name= 'bairro' id='h'/></p>
-					
-				<p><label for='i'>Cidade</label>
-					<input type= 'text'  name= 'cidade' id= 'i'/></p>					
-				
-				<p><label for='s'>Estado</label>
+		<form class="form-horizontal">
+			<fieldset>
+				<legend>
+					Cadastro
+				</legend>
+				<div class="control-group">
+					<label class="control-label">Nome</label>
+					<div class="controls">
+						<input type='text' name='Name' id='a' placeholder="Nome Completo"/>
+					</div>
+				</div>
+
+				<div class="control-group">
+					<label class="control-label">Email</label>
+					<div class="controls">
+						<input type='email' name='email' id='b' placeholder="Email"/>
+
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Senha</label>
+					<div class="controls">
+						<input type='password' name='senha' id='L' placeholder="Senha"/>
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Telefone</label>
+					<div class="controls">
+						<input type='text' name='phone' id='c' placeholder="(38)9121-1232"/>
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Telefone 2</label>
+					<div class="controls">
+						<input type='text' name='phone2' id='d' placeholder="(38)9121-1232"/>
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Data de Nascimento</label>
+					<div class="controls">
+				<input type="text" name='nascimento' id='j' placeholder="12/12/1990"/>
+				</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">CEP</label>
+					<div class="controls">
+					<input type="text" name='cep' id='k' placeholder="39.440-000"/>
+				</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Função</label>
+					<div class="controls">
+					<input type="string" name='funcao' id='l' placeholder="Diretor de marketing"/>
+				</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Endereço</label>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Rua</label>
+					<div class="controls">
+					<input type='text' name='rua' id='e' placeholder="Rua"/>
+				</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Número</label>
+					<div class="controls">
+					<input type='text' name= 'numero' id='numero' placeholder="Número"/>
+				</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Complemento</label>
+					<div class="controls">
+					<input type='text' name= 'complemento' id='g' placeholder="Complemento"/>
+				</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Bairro</label>
+					<div class="controls">
+					<input type='text' name= 'bairro' id='h' placeholder="Bairro"/>
+				</div>
+				</div>
+					<div class="control-group">
+					<label class="control-label">Cidade</label>
+					<div class="controls">
+					<input type= 'text'  name= 'cidade' id= 'i' placeholder="Cidade"/>
+				</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Estado</label>
+					<div class="controls">
 					<select id='s'>
-							
-							<option value="MG">Minas Gerais</option>
-							<option value="AC">Acre</option>
-							<option value="AL">Alagoas</option>
-							<option value="AP">Amapá</option>
-							<option value="AM">Amazonas</option>
-							<option value="BA">Bahia</option>
-							<option value="CE">Ceará</option>
-							<option value="DF">Distrito Federal</option>
-							<option value="ES">Espírito Santo</option>
-							<option value="GO">Goiás</option>
-							<option value="MA">Maranhão</option>
-							<option value="MT">Mato Grosso</option>
-							<option value="MS">Mato Grosso do Sul</option>
-							<option value="PA">Pará</option>
-							<option value="PB">Paraíba</option>
-							<option value="PR">Paraná</option>
-							<option value="PE">Pernambuco</option>
-							<option value="PI">Piauí</option>
-							<option value="RR">Roraima</option>
-							<option value="RO">Rondônia</option>
-							<option value="RJ">Rio de Janeiro</option>
-							<option value="RN">Rio Grande do Norte</option>
-							<option value="RS">Rio Grande do Sul</option>
-							<option value="SC">Santa Catarina</option>
-							<option value="SP">São Paulo</option>
-							<option value="SE">Sergipe</option>
-							<option value="TO">Tocantins</option>
-					</select></p>
-				<input type='submit' value='Salvar' id='salvar'/>	
+
+						<option value="MG">Minas Gerais</option>
+						<option value="AC">Acre</option>
+						<option value="AL">Alagoas</option>
+						<option value="AP">Amapá</option>
+						<option value="AM">Amazonas</option>
+						<option value="BA">Bahia</option>
+						<option value="CE">Ceará</option>
+						<option value="DF">Distrito Federal</option>
+						<option value="ES">Espírito Santo</option>
+						<option value="GO">Goiás</option>
+						<option value="MA">Maranhão</option>
+						<option value="MT">Mato Grosso</option>
+						<option value="MS">Mato Grosso do Sul</option>
+						<option value="PA">Pará</option>
+						<option value="PB">Paraíba</option>
+						<option value="PR">Paraná</option>
+						<option value="PE">Pernambuco</option>
+						<option value="PI">Piauí</option>
+						<option value="RR">Roraima</option>
+						<option value="RO">Rondônia</option>
+						<option value="RJ">Rio de Janeiro</option>
+						<option value="RN">Rio Grande do Norte</option>
+						<option value="RS">Rio Grande do Sul</option>
+						<option value="SC">Santa Catarina</option>
+						<option value="SP">São Paulo</option>
+						<option value="SE">Sergipe</option>
+						<option value="TO">Tocantins</option>
+					</select>
+				</p>
+				<input type='submit' value='Salvar' id='salvar'/>
+			</fieldset>
 		</form>
-		<?php $this->load->view('footer') ?>		
+		<?php $this->load->view('footer') ?>
 	</body>
-	
+
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -2,5 +2,7 @@
 	
 	<head>
 		<title>Sistema de ponto | INFOBITS</title>
-		<script src = "<?php echo base_url('/assets/js/jquery.js')?>"> </script>		
+		<script src = "<?php echo base_url('/assets/js/jquery.js')?>"> </script>
+		<script src = "<?php echo base_url('/assets/js/bootstrap.min.js')?>"> </script>
+		<?php echo link_tag("assets/css/bootstrap.min.css") ?>		
 	</head>
