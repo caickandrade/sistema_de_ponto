dc4bf3c447de572b3b61cbd2088508797e8b550c
Felipe <felipepqm@gmail.com>
2013-09-16 20:10:09 -0300

    Pegando projeto SISTEMA DE PONTO

--- application/controllers/save.php
+++ application/controllers/save.php
@@ -1,4 +1,4 @@
-<<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Save extends CI_Controller {
     
