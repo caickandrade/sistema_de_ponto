b46621643188c9114481614eb91a50334ea9962c
Joana <joanagabrielar@yahoo.com>
2013-09-19 01:38:57 -0300

    agora eu consegui usar o sistem de grid de forma mais adequadaa interface so precisa das mascaras de telefone e a funcao que vai pegar as funcoes do bd.

--- application/views/cadastro.php
+++ application/views/cadastro.php
@@ -1,188 +1,231 @@
+<?php $this->load->view('header')
+?>
 
-<?php $this->load->view('header') ?>
-		
-		
-		
-		<title>Dados Pessoais</title>
-		<!-- <link href="C:/wamp/www/sistema_de_ponto/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"> -->
+<title>Dados Pessoais</title>
+<!-- <link href="C:/wamp/www/sistema_de_ponto/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"> -->
 
-
-	
-	<script src = "<?php echo base_url('/assets/js/cadastro.js')?>" type="text/javascript"> </script>
-	<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.8.1/jquery.validate.min.js" type="text/javascript"></script>
-	<body>
-
+<script src = "<?php echo base_url('/assets/js/cadastro.js')?>" type="text/javascript"></script>
+<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.8.1/jquery.validate.min.js" type="text/javascript"></script>
+<body>
 
 <body>
-<div class="container">
-    
-	<form class="form-inline">
-		<fieldset>
-			<legend>
-				Cadastro
-			</legend>
-			<div class="row-fluid">
-			<div class="span12">
-				<label class="control-label">Nome</label>
-				
-				<input class="span4" type='text' name='Name' id='a' placeholder="Nome Completo" />
-				
-				<label class="control-label">Email</label>
-				
-				<input class="span3" type='email' name='email' id='b' placeholder="Email" />
-				
-				<label class="control-label">Senha</label>
-				
-				<input class="span2" type='password' name='senha' id='L' placeholder="Senha"/><br/><br/>
-				
-				<label class="control-label">Data de Nascimento</label>
-				
-				<input class="span3" type="date" name='nascimento' id='j' placeholder="12/12/1990"/>
-				
-				<label class="control-label">Telefone</label>
-				
-				<input class="span2" type='text' name='phone' id='c' placeholder="(38)9121-1232"/>
-				
-				<label class="control-label">Telefone 2</label>
+	<div class="container">
 
-				<input class="span2" type='text' name='phone2' id='d' placeholder="(38)9121-1232"/><br/><br/>
-
-				<label class="control-label">Função</label>
-
-				<input class="span3" type="text" name='funcao' id='l' placeholder="Diretor de marketing"/><br/><br/>
-				
-				Endereço<br/><br/>
-				
-				<label class="control-label">Estado</label>
-
-				<div class="input-append">
-					<input class="span7" id="estado" name="estado" placeholder="Escolha uma opção" type="text">
-					<div class="btn-group">
-						<button class="btn dropdown-toggle" data-toggle="dropdown">
-							Escolha
-							<span class="caret"></span>
-						</button>
-						<ul class="dropdown-menu">
-
-							<li>
-								<a href="javascript:void(0)" class="estado">Minas Gerais</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Acre</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Alagoas</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Amapá</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Amazonas</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Bahia</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Ceará</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Distrito Federal</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Espírito Santo</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Goiás</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Maranhão</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Mato Grosso</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Mato Grosso do Sul</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Pará</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Paraíba</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Paraná</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Pernambuco</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Piauí</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Roraima</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Rio de Janeiro</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Rio Grande do Norte</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Rio Grande do Sul</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Santa Catarina</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">São Paulo</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Sergipe</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)" class="estado">Tocantis</a>
-							</li>
-						</ul>
-
+		<form class="form-inline">
+			<fieldset>
+				<legend>
+					Cadastro
+				</legend>
+				<div class="row-fluid">
+					<div class="span5">
+						<label class="control-label">Nome</label>
+						<div class="controls">
+							<input class="span10" type='text' name='Name' id='a' placeholder="Nome Completo" />
+						</div>
 					</div>
-
+					<div class="span4">
+						<label class="control-label">Email</label>
+						<div class="controls">
+							<input class="span10" type='email' name='email' id='b' placeholder="Email" />
+						</div>
+					</div>
+					<div class="span3">
+						<label class="control-label">Senha</label>
+						<div class="controls">
+							<input class="span10" type='password' name='senha' id='L' placeholder="Senha"/>
+						</div>
+					</div>
 				</div>
-				<label class="control-label">CEP</label>
+				<div class="row-fluid">
+					<div class="span3">
+						<br/>
+						<label class="control-label">Data de Nascimento</label>
+						<div class="controls">
+							<input class="span10" type="date" name='nascimento' id='j' placeholder="12/12/1990"/>
+						</div>
+					</div>
+					<div class="span3">
+						<br/>
+						<label class="control-label">Telefone</label>
+						<div class="controls">
+							<input class="span10" type='tel' name='phone' id='c' placeholder="(38)9121-1232"/>
+						</div>
+					</div>
+					<div class="span3">
+						<br/>
+						<label class="control-label">Telefone 2</label>
+						<div class="controls">
+							<input class="span10" type='tel' name='phone2' id='d' placeholder="(38)9121-1232"/>
+						</div>
+					</div>
+					<div class="span3">
+						<br/>
+						<label class="control-label">Função</label>
+						<div class="controls">
+							<input class="span10" type="text" name='funcao' id='l' placeholder="Diretor de marketing"/>
+						</div>
+					</div>
+				</div>
+				<br/>
+				Endereço
+				<br/>
+				<div class="row-fluid">
+					<div class="span3">
+						<br/>
+						<label class="control-label">Estado</label>
+						<div class="controls">
+							<div class="input-append">
+								<input class="span8" id="estado" name="estado" placeholder="Escolha uma opção" type="text">
+								<div class="btn-group">
+									<button class="btn dropdown-toggle" data-toggle="dropdown">
+										Escolha
+										<span class="caret"></span>
+									</button>
+									<ul class="dropdown-menu">
 
-				<input class="span2" type="text" name='cep' id='k' placeholder="39.440-000"/>
-				
-				<label class="control-label">Bairro</label>
+										<li>
+											<a href="javascript:void(0)" class="estado">Minas Gerais</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Acre</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Alagoas</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Amapá</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Amazonas</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Bahia</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Ceará</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Distrito Federal</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Espírito Santo</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Goiás</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Maranhão</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Mato Grosso</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Mato Grosso do Sul</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Pará</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Paraíba</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Paraná</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Pernambuco</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Piauí</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Roraima</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Rio de Janeiro</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Rio Grande do Norte</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Rio Grande do Sul</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Santa Catarina</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">São Paulo</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Sergipe</a>
+										</li>
+										<li>
+											<a href="javascript:void(0)" class="estado">Tocantis</a>
+										</li>
+									</ul>
 
-				<input class="span2" type='text' name= 'bairro' id='h' placeholder="Bairro"/>
-				
-				<label class="control-label">Cidade</label>
+								</div>
 
-				<input class="span2" type= 'text'  name= 'cidade' id= 'i' placeholder="Cidade"/><br/><br/>
-				<label class="control-label">Rua</label>
+							</div>
+						</div>
+					</div>
+					<div class="span3">
+						<br/>
+						<label class="control-label">CEP</label>
+						<div class="controls">
+							<input class="span10" type="number" name='cep' id='k' placeholder="39.440-000"/>
+						</div>
+					</div>
+					<div class="span3">
+						<br/>
+						<label class="control-label">Bairro</label>
+						<div class="controls">
+							<input class="span10" type='text' name= 'bairro' id='h' placeholder="Bairro"/>
+						</div>
+					</div>
+					<div class="span3">
+						<br/>
+						<label class="control-label">Cidade</label>
+						<div class="controls">
+							<input class="span10" type= 'text'  name= 'cidade' id= 'i' placeholder="Cidade"/>
+						</div>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span4">
+						<br/>
+						<label class="control-label">Rua</label>
+						<div class="controls">
+							<input class="span10" type='text' name='rua' id='e' placeholder="Rua"/>
+						</div>
+					</div>
+					<div class="span4">
+						<br/>
+						<label class="control-label">Número</label>
+						<div class="controls">
+							<input class="span10" type='number' name= 'numero' id='numero' placeholder="Número"/>
+						</div>
+					</div>
+					<div class="span4">
+						<br/>
+						<label class="control-label">Complemento</label>
+						<div class="controls">
+							<input class="span10" type='text' name= 'complemento' id='g' placeholder="Complemento"/>
+						</div>
+					</div>
+				</div>
 
-				<input class="span3" type='text' name='rua' id='e' placeholder="Rua"/>
+				<div class="row-fluid">
+					<br/>
+					<button id="confirme" name="confirme" class="btn btn-success">
+						Cadastrar
+					</button>
+				</div>
+			</fieldset>
+		</form>
 
-				<label class="control-label">Número</label>
+		<!--<?php $this->load->view('footer') ?> -->
 
-				<input class="span2" type='text' name= 'numero' id='numero' placeholder="Número"/>
-
-				<label class="control-label">Complemento</label>
-
-				<input class="span3" type='text' name= 'complemento' id='g' placeholder="Complemento"/>
-				<label class="control-label">Cadastrar</label>
-
-				<button id="confirme" name="confirme" class="btn btn-success">
-					Confirme
-				</button>
-			</div>
-
-		</fieldset>
-	</form>
-
-	<!--<?php $this->load->view('footer') ?> -->
-</div>
-</div>
-</div>
+	</div>
 </body>
 
 </html>
