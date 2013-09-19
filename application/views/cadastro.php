2bf8159c14049f233c60f1a097c382257e577e9d
Joana <joanagabrielar@yahoo.com>
2013-09-19 00:19:40 -0300

    modifiquei o cadastro para minimizar o espao ocupado por ele Luciano ficou de alterar os Ids.

--- application/views/cadastro.php
+++ application/views/cadastro.php
@@ -12,82 +12,51 @@
 	<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.8.1/jquery.validate.min.js" type="text/javascript"></script>
 	<body>
 
-		<form class="form-horizontal formCadastro" id="formu" method="post" >
-			<fieldset>
-				<legend>
-					Cadastro
-				</legend>
-				<div class="control-group">
-					<label class="control-label" for="Name">Nome</label>
-					<div class="controls">
-						<input type='text' name='Name' id='Name' placeholder="Nome Completo" class="required isBlanck" />
-					</div>
-				</div>
-
-<title>Dados Pessoais</title>
-<!-- <link href="C:/wamp/www/sistema_de_ponto/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"> -->
-
-
-<script src = "<?php echo base_url('/assets/js/cadastro.js')?>"></script>
-
-				<div class="control-group">
-					<label class="control-label" for="email">Email</label>
-					<div class="controls">
-						<input type='email' name='email' id='email' placeholder="Email" class="required isBlanck" />
-
 
 <body>
 <div class="container">
-	 <div class="row">
-    <div class="span8 offset2">
+    
 	<form class="form-inline">
 		<fieldset>
 			<legend>
 				Cadastro
 			</legend>
-			<div class="control-group ">
+			<div class="row-fluid">
+			<div class="span12">
 				<label class="control-label">Nome</label>
-
-
-				<input class="span4" type='text' name='Name' id='a' placeholder="Nome Completo" class="isBlanck" />
-				&nbsp &nbsp<label class="control-label">Email</label>
-
-				<input type='email' name='email' id='b' placeholder="Email" class="isBlanck" />
-			</div>
-
-			<div class="control-group">
-
+				
+				<input class="span4" type='text' name='Name' id='a' placeholder="Nome Completo" />
+				
+				<label class="control-label">Email</label>
+				
+				<input class="span3" type='email' name='email' id='b' placeholder="Email" />
+				
 				<label class="control-label">Senha</label>
-				<input type='password' name='senha' id='L' placeholder="Senha"/>
+				
+				<input class="span2" type='password' name='senha' id='L' placeholder="Senha"/><br/><br/>
+				
 				<label class="control-label">Data de Nascimento</label>
-				<input type="date" name='nascimento' id='j' placeholder="12/12/1990"/>
-
-			</div>
-
-			<div class="control-group">
+				
+				<input class="span3" type="date" name='nascimento' id='j' placeholder="12/12/1990"/>
+				
 				<label class="control-label">Telefone</label>
-
-				<input type='text' name='phone' id='c' placeholder="(38)9121-1232"/>
+				
+				<input class="span2" type='text' name='phone' id='c' placeholder="(38)9121-1232"/>
+				
 				<label class="control-label">Telefone 2</label>
 
-				<input type='text' name='phone2' id='d' placeholder="(38)9121-1232"/>
+				<input class="span2" type='text' name='phone2' id='d' placeholder="(38)9121-1232"/><br/><br/>
 
-			</div>
-
-			<div class="control-group">
 				<label class="control-label">Função</label>
 
-				<input type="text" name='funcao' id='l' placeholder="Diretor de marketing"/>
-
-			</div>
-
-			<label class="control-label">Endereço</label>
-
-			<div class="control-group">
+				<input class="span3" type="text" name='funcao' id='l' placeholder="Diretor de marketing"/><br/><br/>
+				
+				Endereço<br/><br/>
+				
 				<label class="control-label">Estado</label>
 
 				<div class="input-append">
-					<input id="estado" name="estado" class="span2" placeholder="Escolha uma opção" type="text">
+					<input class="span7" id="estado" name="estado" placeholder="Escolha uma opção" type="text">
 					<div class="btn-group">
 						<button class="btn dropdown-toggle" data-toggle="dropdown">
 							Escolha
@@ -180,41 +149,31 @@
 				</div>
 				<label class="control-label">CEP</label>
 
-
-				<input type="text" name='cep' id='k' placeholder="39.440-000"/>
-
-			</div>
-
-			<div class="control-group">
-				<label class="control-label">Cidade</label>
-
-				<input type= 'text'  name= 'cidade' id= 'i' placeholder="Cidade"/>
-				<label class="control-label">Rua</label>
-
-				<input type='text' name='rua' id='e' placeholder="Rua"/>
-
-			</div>
-
-			<div class="control-group">
-				<label class="control-label">Número</label>
-
-				<input type='text' name= 'numero' id='numero' placeholder="Número"/>
+				<input class="span2" type="text" name='cep' id='k' placeholder="39.440-000"/>
+				
 				<label class="control-label">Bairro</label>
 
-				<input type='text' name= 'bairro' id='h' placeholder="Bairro"/>
+				<input class="span2" type='text' name= 'bairro' id='h' placeholder="Bairro"/>
+				
+				<label class="control-label">Cidade</label>
 
-			</div>
+				<input class="span2" type= 'text'  name= 'cidade' id= 'i' placeholder="Cidade"/><br/><br/>
+				<label class="control-label">Rua</label>
 
-			<div class="control-group">
+				<input class="span3" type='text' name='rua' id='e' placeholder="Rua"/>
+
+				<label class="control-label">Número</label>
+
+				<input class="span2" type='text' name= 'numero' id='numero' placeholder="Número"/>
+
 				<label class="control-label">Complemento</label>
 
-				<input type='text' name= 'complemento' id='g' placeholder="Complemento"/>
+				<input class="span3" type='text' name= 'complemento' id='g' placeholder="Complemento"/>
 				<label class="control-label">Cadastrar</label>
 
-				<button id="confirme" name="confirme" class="btn btn-info">
+				<button id="confirme" name="confirme" class="btn btn-success">
 					Confirme
 				</button>
-
 			</div>
 
 		</fieldset>
