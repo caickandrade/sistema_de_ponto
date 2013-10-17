33e94bb35dc0474773c1b5fa2f03a22bb6952c58
Luciano <luciano-antunes@hotmail.com>
2013-10-17 19:54:42 -0300

    f5

--- application/views/login.php
+++ application/views/login.php
@@ -17,7 +17,7 @@
 							<img src="assets/img/teste.jpg" class="img-rounded">
 						</div> 
 					</div>
-					<form class="container" id='formlog'>
+					<div class="container" id='formlog'>
 						
 					<div class="span9">
 						<legend>
@@ -46,7 +46,7 @@
 							</button>
 						</div>
 					</div>
-					</form>
+					</div>
 				</div>
 			</div>
 		</body>
