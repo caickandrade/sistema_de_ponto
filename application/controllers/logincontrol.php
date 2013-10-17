bc0886515c8e9eee5de45f6c99bca189e48ba9ff
Luciano <luciano-antunes@hotmail.com>
2013-10-17 20:39:54 -0300

    loginn

--- application/controllers/logincontrol.php
+++ application/controllers/logincontrol.php
@@ -29,6 +29,7 @@ class LoginControl extends CI_Controller {
 			//se o retorno da função for 1 é porque o usuário ou a senha estão incorretos
 			if($retornoVerifyLogin == 1)
 			{
+				$this->load->view('cadastro');	
 			    $response = array(
 					"msg" => "usuario e senha incorretos"
 				);
