e854a523b395154a73dcf0a8dccfd3ff83ebb4bf
Luciano <luciano-antunes@hotmail.com>
2013-10-31 19:52:00 -0200

    atualizar de novo

---
+++ application/views/busca.php
@@ -0,0 +1,17 @@
+<?php $this->load->view('header')
+?>
+<head>
+	
+	<title>Busca</title>
+
+</head>
+
+	<input type="text" class="text-search span5" />	
+	<input type="image" src="<?php echo base_url('/assets/img/search.png')?>" alt="Submit" class="searchButton">
+
+<body>
+
+	
+</body>
+
+</html>
