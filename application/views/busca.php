acd7eeecae6ff087f2f4b591aceac83047824afd
Luciano <luciano-antunes@hotmail.com>
2013-11-07 19:50:55 -0200

    e

--- application/views/busca.php
+++ application/views/busca.php
@@ -4,11 +4,32 @@
 	
 	<title>Busca</title>
 
-</head>
+</head>cd
 
-	<input type="text" class="text-search span5" />	
-	<input type="image" src="<?php echo base_url('/assets/img/search.png')?>" alt="Submit" class="searchButton">
-
+	<div class="search">
+		Buscar
+		<select class="filtro span3">
+		<option value="1">Nome</option>
+		<option value="2">CPF</option>
+		<option value="3">E-mail</option>									
+		</select>
+		<input type="text" class="text-search span5" />	
+		<input type="image" src="<?php echo base_url('/assets/img/search.png')?>" alt="Submit" class="searchButton">															
+	</div>
+		<!-- Modal -->
+	<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+			<h3 id="myModalLabel">Título do modal</h3>
+		</div>
+		<div class="modal-body">
+		<p>E aqui o conteúdo...</p>
+	  	</div>
+	  	<div class="modal-footer">
+			<button class="btn" data-dismiss="modal" aria-hidden="true">Fechar</button>
+			<button class="btn btn-primary">Salvar</button>
+	  	</div>
+	</div>
 <body>
 
 	
