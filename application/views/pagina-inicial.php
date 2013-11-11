980bb7c3650719617aae498bce30a3a35703495c
Felipe Alves <oliveira.felipealves@gmail.com>
2013-11-11 20:55:44 -0200

    modal

--- application/views/pagina-inicial.php
+++ application/views/pagina-inicial.php
@@ -10,6 +10,21 @@
 	<div class="cotainer">
 		<?php $this->load->view('menu') ?>	
 	</div>
+	<!-- Button to trigger modal -->
+	<a href="#myModal" role="button" class="btn" data-toggle="modal">Executar modal de demo</a>
+	<!-- Modal -->
+	<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  		<div class="modal-header">
+    		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    		<h3 id="myModalLabel">Aviso</h3>
+  		</div>
+  		<div class="modal-body">
+    		<p>Um aviso</p>
+  		</div>
+  		<div class="modal-footer">
+    		<button class="btn" data-dismiss="modal" aria-hidden="true">Fechar</button>
+  		</div>
+	</div>
 	
 
 </body>
