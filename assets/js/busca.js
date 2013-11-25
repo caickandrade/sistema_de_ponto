30238664539d20b733febc1a7f1b1becc3ea91fd
Caick <caila@ig.com.br>
2013-11-25 21:27:34 -0200

    autocomplete testando de novo

--- assets/js/busca.js
+++ assets/js/busca.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-		actions();
+		activia();
 	});
 	
 	
-function actions(){
+function activia(){
 	
-	    $( "#buscar" ).typeahead({
+	    $('#buscar').typeahead({
 	             name: 'buscarUser',
 	             remote: '/sistema_de_ponto/index.php/busca/getUser?query=%QUERY',
 	             minLenght: 3,
-	             limit: 5
+	             limit: 6
         });
-	
-};
+        
+}
