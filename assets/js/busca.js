2b0e5271aa3f45a54f09ba54bdcbebbb2df8180f
Caick <caila@ig.com.br>
2014-02-25 20:57:25 -0300

    autocomplete voltou

--- assets/js/busca.js
+++ assets/js/busca.js
@@ -1,11 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function() {	
 		actions();
 	});
 	
 	
 function actions(){
-	
-	    $('#buscar').typeahead({
+	    $('#procurar').typeahead({	    
 	             name: 'buscarUser',
 	             remote: '/sistema_de_ponto/index.php/busca/getUserAutoComplete?query=%QUERY',
 	             minLenght: 3,
