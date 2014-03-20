4fd6bbe0d6d2bc9b41054f38efb885af80d4aed1
Luciano <luciano-antunes@hotmail.com>
2014-03-20 20:36:50 -0300

    to revoltado

--- application/views/relatoriogeral.php
+++ application/views/relatoriogeral.php
@@ -10,7 +10,7 @@
 	</head>
 	
 	<div class="container">
-		<?php $this->load->view('menu') ?>
+		<?php $this->load->view('menu')?>
 	</div>
 	
 	<body> 
@@ -19,17 +19,93 @@
 			<div class="row-fluid">
 				<div class="span12">
 					
+					
+					<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script type="text/javascript">
+$(function () {
+        $('#container').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Historic World Population by Region'
+            },
+            subtitle: {
+                text: 'Source: Wikipedia.org'
+            },
+            xAxis: {
+                categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+                title: {
+                    text: null
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Population (millions)',
+                    align: 'high'
+                },
+                labels: {
+                    overflow: 'justify'
+                }
+            },
+            tooltip: {
+                valueSuffix: ' millions'
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true
+                    }
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'top',
+                x: -40,
+                y: 100,
+                floating: true,
+                borderWidth: 1,
+                backgroundColor: '#FFFFFF',
+                shadow: true
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: 'Year 1800',
+                data: [107, 31, 635, 203, 2]
+            }, {
+                name: 'Year 1900',
+                data: [133, 156, 947, 408, 6]
+            }, {
+                name: 'Year 2008',
+                data: [973, 914, 4054, 732, 34]
+            }]
+        });
+    });
+    
+
+		</script>
+					
+					
+					
+					
+					<!--
 					<div id="cadastro">
-						
+					
+										
 							<label class="control-label">Gráfico do Relatório Geral</label>
 							
 							<div class="progress progress-success">
-  								<div class="bar" style="width: 40%"></div>
+  								<div class="bar" style="width: 70%"></div>
 							</div>
-							
-							<span class="label label-success"><div id="reducao">..........</div></span><label id="comentario">Horas trabalhadas </label>
+							<div id="al">
+								<span class="label label-success"><div id="reducao">..........</div></span><label id="comentario">Horas trabalhadas </label>
+							</div>
 					</div>	
-
+					-->
 				</div>
 			</div>
 		</div>
